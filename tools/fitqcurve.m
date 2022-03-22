f96ba78b8a2ec39b5388c01c4feea1ac30804877
jonathanccfried <97637080+jonathanccfried@users.noreply.github.com>
2022-03-21 20:34:17 -0400

    Add files via upload

---
+++ tools/fitqcurve.m
@@ -0,0 +1,28 @@
+clear
+
+q = readmatrix('qcurve.txt');
+q = q';
+
+p = readmatrix('pcurve.txt');
+pcartesian = (p(:,1:3))';
+l = lambda_calc(pcartesian);
+n = 15; %order of the poly
+
+for i=1:size(q,1)
+    a(i,:) = polyfit(l,q(i,:),n);
+    qfit(i,:) = polyval(a(i,:),l);
+    err(i) = norm(q(i,:)-qfit(i,:));
+    figure(i)
+    plot(l,q(i,:),l,polyval(a(i,:),l))
+    legend(strcat('q' , num2str(i)),'qfit')
+    aprime(i,:) = polyder(a(i,:));
+    adoubleprime(i,:) = polyder(aprime(i,:));
+    qprime(i,:) = polyval(aprime(i,:),l);
+    qdoubleprime(i,:) = polyval(adoubleprime(i,:),l);
+end
+err
+
+qdotmax = [1.75;1.57;1.57;2.97;2.09;3.32]; % joint speed limits in rad/s
+qdotmin = -qdotmax;
+qddotmax = (pi/180)*[312;292;418;2407;1547;3400]; %joint acceleration limits in rad/s^2. Reasonable guess that it takes 0.1 seconds for a joint to reach its max limit
+qddotmin = -qddotmax;
